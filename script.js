73431885531434c99a9c700b3cf7fccbba07d306
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-08-11 11:12:29 -0300

    trying validate email

--- script.js
+++ script.js
@@ -7,33 +7,67 @@ const main_message = document.querySelector('.success')
 const emailInput = document.querySelector('#iemail')
 const emailForm = document.querySelector('#emailForm')
 
-// Event that controls after submit of form
+const personEmail = document.querySelector('.personEmail')
 
-emailForm.addEventListener("submit", function(event) {
+/* Event that controls after submit of form
+
+emailForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const email = emailInput.value;
-    const isValid = validateEmail(email)
-    // validate email
-
+    
+    const isValid = validateEmail(email);
+    
     if(isValid) {
-        btn_subscribe.addEventListener('click', () => {
-            main_card.classList.toggle('dn')
-            main_message.classList.toggle('db')
-        })
-        
-        btn_dimiss.addEventListener('click', () => {
-            main_card.classList.toggle('dn')
-            main_message.classList.toggle('db')
-        })
+        unblockBtn();
+        personEmail.innerHTML = email
     } else {
-        
+        emailForm.classList.add('errorEmail')
     }
+
 })
 
-// validate function
+// function validate email
 
 const validateEmail = (email) => {
-    const regex = /^[^\s]+@[^\s]+\.[^\s]+$/
+    const regex = /^[^\s]+@[^\s]+.[^\s]+$/;
     return regex.test(email);
 }
+
+// function unblock the button
+
+const unblockBtn = () => {
+    main_card.classList.toggle('dn')
+    main_message.classList.toggle('db')
+}
+
+function homePage() {
+    unblockBtn();
+}
+
+*/
+
+
+// another tip to validate email
+
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+const requiredEmail = document.querySelector('.required')
+
+const setError = () => {
+    requiredEmail.style.border = '1px solid red'
+}
+
+const removeError = () => {
+    requiredEmail.style.border = ''
+}
+
+const emailValidate = () => {
+    if(requiredEmail.value.length < 3) {
+        setError();
+    } else {
+        removeError();
+    }
+}
+
+
+
 
